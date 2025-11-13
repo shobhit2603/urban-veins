b053c611eed1b3409befc15f121557c1c49c5a71
shobhit2603 <shrivastavashobhit706@gmail.com>
2025-11-14 00:51:57 +0530

    Initialized shandcn-ui

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,14 @@ import "./globals.css";
 export const metadata = {
   title: "Urban Veins",
   description: "A clothing store for the modern urbanite.",
+  icons: {
+    icon: "/favicon.png",
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="smooth-scroll">
       <body className="antialiased">
         {children}
       </body>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <>
+    <main>
       <h1 className="text-6xl font-bold text-center font-[ClashDisplay-Regular]">Urban Veins</h1>
-    </>
+    </main>
   );
 }
